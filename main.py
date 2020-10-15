15ccc750efd5d83d337e6e0bb0b95fde237e09ef
Sarah Rose <sarah@lavenderlink.com>
2020-10-15 09:19:11 -0400

    The second fix

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-digit1 = input("put your number in")
-digit2 = input("put your other number in")
-mtype = input("What function do you want to do")
+digit1 = input("put your number in -")
+digit2 = input("put your other number in -")
+mtype = input("What function do you want to do?")
 
 if mtype == "+":
-    print(digit1 + digit2)
+    print(float(digit1) + float(digit2))
 elif mtype == "-":
-    print(digit1 - digit2)
+    print(float(digit1) - float(digit2))
 elif mtype == "/":
-    print(digit1 / digit2)
+    print(float(digit1) / float(digit2))
 elif mtype =="*":
-    print(digit1 * digit2)
+    print(float(digit1) * float(digit2))
 else:
     print("Your function is wrong do it again")
